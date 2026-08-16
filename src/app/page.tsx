e16315b3f6635fbac43d1b098d7f825e6049a7e6
1STERI4KA <gurgen4ik228666@gmail.com>
2026-08-17 01:30:31 +0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { prisma } from "@/lib/prisma";
-
+export const dynamic = "force-dynamic";
 export default async function DashboardPage() {
   const players = await prisma.player.findMany({
     where: { isActive: true },
